6557412140d36f7d25e923a0e0a537e95e820136
My Name <iamsunnysingh783@gmail.com>
2024-12-29 14:46:32 +0530

    commiting the merge

--- routes/Page.js
+++ routes/Page.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { createPage } = require("../controllers/Page");
+const {auth}=require("../middlewares/auth");
 const router = express.Router();
 
-router.post("/createpage", createPage);
+router.post("/createpage",auth, createPage);
 
 module.exports = router;
