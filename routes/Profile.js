82fe8f225b6deb22cb3d2c51430579c70e43d685
My Name <iamsunnysingh783@gmail.com>
2025-03-02 22:02:25 +0530

    some changes in backend

--- routes/Profile.js
+++ routes/Profile.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router=express.Router();
 const {auth}=require("../middlewares/auth");
-const {updateDisplayPicture,updateProfile,deleteAccount,}=require("../controllers/Profile");
+const {updateDisplayPicture,updateProfile,deleteAccount,getAllUserDetails}=require("../controllers/Profile");
 
 //====================================================
 //this is for changing the profile
 router.delete("/deleteProfile",auth,deleteAccount);
 router.put("/updateProfile", auth, updateProfile);
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+router.get("/getuserdetail",auth,getAllUserDetails);
 
 module.exports=router;
