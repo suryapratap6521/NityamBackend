fcaeb75137269bbbb287a72e4abbc3bbedf66eca
risxbh <82149801+risxbh@users.noreply.github.com>
2025-04-17 17:43:09 +0530

    Payment Updates

--- models/AdvertisedPost.js
+++ models/AdvertisedPost.js
@@ -19,7 +19,7 @@ const replySchema = new mongoose.Schema({
       ref: "User",
     },
   ],
-  replies: [this], 
+  replies: [this],
 });
 
 // Comment Schema
@@ -42,7 +42,7 @@ const commentSchema = new mongoose.Schema({
       ref: "User",
     },
   ],
-  replies: [replySchema], 
+  replies: [replySchema],
 });
 
 // Advertised Post Schema
@@ -53,62 +53,66 @@ const advertisedPostSchema = new mongoose.Schema(
       required: true,
     },
     imagesArray: {
-      type: [String], 
+      type: [String],
     },
     // timeSlot: {
     //     start: { type: Date, required: true },
     //     end: { type: Date, required: true },
     // },
     ageGroup: {
-        minAge: { type: Number, required: true },
-        maxAge: { type: Number, required: true },
+      minAge: { type: Number, required: true },
+      maxAge: { type: Number, required: true },
     },
-    pageId:{
-      type:mongoose.Schema.Types.ObjectId,ref:"Page"
+    pageId: {
+      type: mongoose.Schema.Types.ObjectId, ref: "Page"
     },
 
     dateSlot: {
-        startDate: { type: Date, required: true },
-        endDate: { type: Date, required: true },
+      startDate: { type: Date, required: true },
+      endDate: { type: Date, required: true },
     },
     communities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Community' }],
     premium: { type: Boolean, default: false },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User", 
+      ref: "User",
       required: true,
     },
     buttonLabel: {
-        type: {
-          type: String,
-          enum: [
-            "Contact Us",
-            "Send Enquiry",
-            "Apply Now",
-            "Visit Our Website",
-            "Message Us",
-            "Download App",
-            "Subscribe Us",
-            "Fill Form"
-          ],
-          required: true,
-        },
-        value: {
-          type: String,
-          required: true,
-        },
+      type: {
+        type: String,
+        enum: [
+          "Contact Us",
+          "Send Enquiry",
+          "Apply Now",
+          "Visit Our Website",
+          "Message Us",
+          "Download App",
+          "Subscribe Us",
+          "Fill Form"
+        ],
+        required: true,
       },
-      
+      value: {
+        type: String,
+        required: true,
+      },
+    },
+
     like: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
     ],
-    comments: [commentSchema], 
+    comments: [commentSchema],
     createdAt: {
       type: Date,
       default: Date.now,
+    },
+    transactionId: {
+      type: String,
+      required: false,
     },
   },
   { timestamps: true }
